cd1f916782ee2fff58192113700f87fd427e7662
Matthew Shen <mds377@cornell.edu>
2023-10-13 10:29:42 -0400

    finish problem 1 and 2a

--- problems/problem_1/p1_b.py
+++ problems/problem_1/p1_b.py
@@ -2,4 +2,42 @@
 
 
 def maxsum_tree(vertices, adjacency) -> int:
-    pass
+    def max_sum_func(node, parent_selected, dp):
+    	if dp[(node, parent_selected)] != None:
+    		return dp[(node, parent_selected)]
+    
+    	node_max = 0
+    
+    	if not parent_selected:
+            with_node = vertices[node] + sum(max_sum_func(child, True, dp) for child in adjacency[node])
+            with_out_node = sum(max_sum_func(child, False, dp) for child in adjacency[node])
+            node_max = max(with_node,with_out_node)
+    	else:
+            node_max = sum(max_sum_func(child, 0, dp) for child in adjacency[node])
+    
+    	dp[(node, parent_selected)] = node_max
+    	return node_max
+
+    def get_root(adjacency):
+        children_lst = []
+        for values_lst in adjacency.values():
+            children_lst += values_lst
+
+        for node in adjacency.keys():
+            if node not in children_lst:
+                root = node
+        return root
+    
+
+    
+    # Initialization of the dp
+    dp = {}
+    for node in vertices:
+        for parent_bool in [True, False]:
+            dp[(node, parent_bool)] = None
+
+    # calling the solve function for root with parent not selected
+    max_sum = max_sum_func(get_root(adjacency), False, dp)
+    
+    return max_sum
+        
--- problems/problem_2/p2_a.py
+++ problems/problem_2/p2_a.py
@@ -8,4 +8,23 @@ def d(point1, point2):
     return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
 
 def closest_pair(points):
-    pass
+    min_points = [(0,0),(0,0)]
+    min_distance = float('inf')
+    for point_1 in points:
+        for point_2 in points:
+            if point_1 != point_2:
+                if d(point_1,point_2) < min_distance:
+                    min_points = [point_1,point_2]
+                    min_distance = d(point_1,point_2)
+
+    return min_points
+
+if __name__ == '__main__':
+    input_2 = [
+    [0.47, 0.53],
+    [0.99, 0.54],
+    [0.4,  0.57],
+    [0.83, 0.19],
+    [0.73, 0.1 ],]
+
+    print(closest_pair(input_2))
